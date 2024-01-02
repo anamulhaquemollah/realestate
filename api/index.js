fc4a4705f396eeedea94dcdfe9cbcb63cee17d41
anamulhaquemollah <anamul1414@gmail.com>
2024-01-02 13:56:40 +0600

    connect to db

--- api/index.js
+++ api/index.js
@@ -1,8 +1,16 @@
 import express from "express";
+import mongoose from "mongoose";
+//by default you can't use env variable in backend. you need another package to do that
+import dotenv from "dotenv"; 
+
+dotenv.config(); 
+mongoose.connect(process.env.MONGO)
+.then(()=>console.log("connected to db"))
+.catch((err)=>console.log(err))
 
 const app = express(); 
 
 app.listen(3000, ()=>{
-    console.log("Server is running on port 3000!!!"); 
+    console.log("Server is running on port 3000!!"); 
 }); 
 
