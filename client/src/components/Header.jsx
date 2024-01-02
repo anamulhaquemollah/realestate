fcafead5a1bc23e3f49091587393322d7d17c91e
anamulhaquemollah <anamul1414@gmail.com>
2024-01-02 12:55:25 +0600

    api initiated

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import {FaSearch} from "react-icons/fa"; 
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <header>
-        
+    <header className='bg-slate-200 shadow-md'>
+       <div className='flex justify-between items-center max-w-6xl mx-auto p-3 flex-wrap'>
+        <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
+        <span className='text-slate-500'>Real</span>
+        <span className='text-slate-700'>Estate</span>
+       </h1>
+       <form className="bg-slate-100 flex items-center p-3 rounded-lg">
+        <input type="text" 
+        placeholder='Search...' 
+        className='bg-transparent  outline-none w-24 sm:w-64'/>
+        <FaSearch className="text-slate-700"/>
+       </form>
+
+       <ul className="flex gap-3">
+          <Link to='/'>
+            <li className="hidden sm:inline text-slate-700">Home</li>
+          </Link>
+          <Link to='/about'>
+            <li className="hidden sm:inline text-slate-700">About</li>
+          </Link>
+          <Link to='/sign-in'>
+            <li className="text-slate-700">Sign-In</li>
+          </Link>
+        </ul>
+       </div>
     </header>
   )
 }
