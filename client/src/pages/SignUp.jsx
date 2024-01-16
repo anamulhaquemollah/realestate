9fc784c30773abf6f87dc10a471a50c84e051fbe
Anamul Haque <anamul1414@gmail.com>
2024-01-17 03:43:21 +0600

    signup functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,14 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 const SignUp = () => {
+
+  const [formData, setFormData] = useState({}); 
+  const changeHandler = (e)=>{
+    setFormData({
+      ...formData, 
+      [e.target.id]: e.target.value
+    })
+  }
+
+  const submitHandler =  async (e)=>{
+    e.preventDefault(); 
+
+    const res = await fetch("/api/auth/signup", {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json', 
+      }, 
+      body: JSON.stringify(formData)
+    });
+    console.log(res);
+    const data = await res.json(); 
+    console.log(data);
+  }
+console.log(formData); 
   return (
     <div className='max-w-xl p-10 mx-auto'>
       <h1 className='text-3xl text-center font-bold'>Sign Up</h1>
-      <form className='flex flex-col gap-5 p-8'>
-        <input type="text" placeholder='username' className='border-2 outline-none p-3 rounded-lg' id='username'/>
-        <input type="email" placeholder='email' className='border-2 outline-none p-3 rounded-lg' id='email'/>
-        <input type="password" placeholder='password' className='border-2 outline-none p-3 rounded-lg' id='password'/>
+      <form onSubmit={submitHandler} className='flex flex-col gap-5 p-8'>
+        <input type="text" placeholder='username' className='border-2 outline-none p-3 rounded-lg' id='username' onChange={changeHandler} />
+        <input type="email" placeholder='email' className='border-2 outline-none p-3 rounded-lg' id='email'onChange={changeHandler} />
+        <input type="password" placeholder='password' className='border-2 outline-none p-3 rounded-lg' id='password' onChange={changeHandler} />
         <button className='border p-3 bg-slate-700 rounded-lg text-white uppercase hover:bg-slate-600 font-bold'>Sign up</button>
         <div className='flex gap-2'>
         <p>Have an account?</p>
--- client/vite.config.js
+++ client/vite.config.js
@@ -5,10 +5,16 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173,
-    strictPort: true,
-    hmr: {
-      port: 5173,
+    proxy: {
+      'api': {
+        target: 'http://localhost:3000', 
+        secure: false,
+      }
     },
+    // port: 5173,
+    // strictPort: true,
+    // hmr: {
+    //   port: 5173,
+    // },
   },
 })
